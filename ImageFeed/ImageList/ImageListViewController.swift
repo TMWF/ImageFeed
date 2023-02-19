7f1e4bac0a10bc776203d0b9ad5f39ff50cd74c9
TMWF <81715038+TMWF@users.noreply.github.com>
2023-02-19 22:48:21 +0300

    Sprint 09 (#2)
    * ProfileViewController added
    
    * Added TabBar and NavigationController
    
    * Added TabBar and NavigationController
    
    * added segue
    
    * Added Scrollview
    
    * UI programmatically in profile view controller
    
    * Minor chages
    
    * Minor fixes after review

--- ImageFeed/ImageList/ImageListViewController.swift
+++ ImageFeed/ImageList/ImageListViewController.swift
@@ -22,14 +22,25 @@ class ImageListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationController?.isNavigationBarHidden = true
         tableview.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Constants.segueToSingleImageViewController {
+            let viewController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
 }
 
 extension ImageListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: Constants.segueToSingleImageViewController, sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Leo Bonhart on 18.02.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet private var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
+    
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction private func didTapShareButton(_ sender: UIButton) {
+        guard let image else { return }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(vc, animated: true)
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+}
