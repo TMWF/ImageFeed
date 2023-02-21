8f0c0a03f344c809a0ec918c02dcc1f23f22dab1
Joe <gpliev777@gmail.com>
2023-02-21 23:24:47 +0300

    Added API constants

--- ImageFeed/Helpers/Constants.swift
+++ ImageFeed/Helpers/Constants.swift
@@ -8,6 +8,14 @@
 import Foundation
 
 struct Constants {
+    static let defaultBaseURL: URL = {
+        guard let url = URL(string: "https://api.unsplash.com") else { fatalError("Error occured while trying to create base api URL")}
+        return url
+    }()
+    static let accessScope = "public+read_user+write_likes"
+    static let accessKey = "bTvAmkPrW1p5aHFxCQNRzgFL8OwygTj0t70Qryk2t98"
+    static let secretKey = "irOnn5eGjJ08UWZKIm3NrlgYaIPIoPgb3DleuhzMnic"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let likeImageOn = "like_button_on"
     static let likeImageOff = "like_button_off"
     static let segueToSingleImageViewController = "ShowSingleImage"
